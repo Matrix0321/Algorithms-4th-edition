a1837658b70d7153cb77440b80e27a0e5270d47e
Matrix0321 <44547719+Matrix0321@users.noreply.github.com>
2022-08-25 00:23:53 +0800

    Create Ex13.java

---
+++ Chapter1-1/Ex13.java
@@ -0,0 +1,28 @@
+Code:
+
+package algo;
+
+public class Ex1_13 {
+	public static void printTransposedMatrix(int[][]matrix) {
+		for(int i=0;i<matrix[0].length;i++) {
+			for(int j=0;j<matrix.length;j++) {
+				System.out.printf("%4d", matrix[j][i]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[][]a= {{1,2,3},{4,5,6}};
+		printTransposedMatrix(a);
+	}
+
+}
+
+
+
+Output:
+
+   1   4
+   2   5
+   3   6
